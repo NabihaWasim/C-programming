c92181352433d3ef58f7a798cb6542942fc207bb
Nabiha Wasim <nabihawasim1508@gmail.comgit config --global user.name>
2026-07-17 16:15:08 +0530

    Added structure program

---
+++ 09_structure.c
@@ -0,0 +1,29 @@
+/*
+Program: Creation of structure
+Language: C
+Author: Nabiha Wasim
+Description: This program creates structure and declare 2 variable
+and prints the name of first and age of others.
+*/
+#include<stdio.h>
+#include<string.h>
+int main(){
+    struct Person{
+        char name[50];
+        int salary;
+        int age;
+    }A,B,C;
+    A.age=32;
+    A.salary=45000; //to copy the name into string
+    strcpy(A.name,"Mohan");
+    B.age=34;
+    B.salary=50000;
+    strcpy(B.name,"Albert Guru");
+    C.age=28;
+    C.salary=55000;
+    strcpy(C.name,"Harry");
+    printf("%s\n",A.name);
+    printf("%d\n",B.age);
+    printf("%d\n",C.age);
+    return 0;    
+}
